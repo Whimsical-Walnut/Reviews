ba0da94a66b746df41133b12e8356481a66f79c9
Momo Sun <mosun1113@gmail.com>
2021-03-16 22:54:10 -0700

    fix mysql schema naming

--- Database/schema.sql
+++ Database/schema.sql
@@ -17,19 +17,19 @@ CREATE TABLE product (
 
 CREATE TABLE review (
   id INT NOT NULL AUTO_INCREMENT,
-  productId INT NOT NULL,
-  email VARCHAR(50) NOT NULL ,
-  name VARCHAR(45) NOT NULL ,
+  product_id INT NOT NULL,
+  reviewer_name VARCHAR(255) NOT NULL,
+  reviewer_email VARCHAR(50) NOT NULL ,
   rating INT NOT NULL CHECK (rating<6 AND rating>=0),
   recommend TINYINT(1) DEFAULT NULL,
   summary VARCHAR(255) NOT NULL ,
   helpfulness INT DEFAULT NULL ,
   reponse VARCHAR(255) NOT NULL,
-  report INT DEFAULT NULL,
+  reported INT DEFAULT NULL,
   body VARCHAR(255) NOT NULL,
   date DATETIME NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (productId)
+  FOREIGN KEY (product_id)
     REFERENCES product(id)
     ON DELETE CASCADE
   );
@@ -38,10 +38,10 @@ CREATE TABLE review (
 
 CREATE TABLE photos(
   id INT NOT NULL AUTO_INCREMENT,
-  reviewId INT NOT NULL,
+  review_id INT NOT NULL,
   url VARCHAR(50) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (reviewId)
+  FOREIGN KEY (review_id)
     REFERENCES review(id)
     ON DELETE CASCADE
 );
@@ -50,10 +50,10 @@ CREATE TABLE photos(
 
   CREATE TABLE characteristics(
   id INT NOT NULL AUTO_INCREMENT,
-  productId INT NOT NULL,
+  product_id INT NOT NULL,
   name VARCHAR(255) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (productId)
+  FOREIGN KEY (product_id)
 	  REFERENCES product(id)
 	  ON DELETE CASCADE
   );
@@ -61,11 +61,11 @@ CREATE TABLE photos(
 
   CREATE TABLE characteristics_reviews(
   id INT NOT NULL AUTO_INCREMENT,
-  characteristicsId INT NOT NULL,
+  characteristic_id INT NOT NULL,
   reviewId INT NOT NULL,
   value DECIMAL(5,4) NOT NULL,
   PRIMARY KEY (id),
-  FOREIGN KEY (characteristicsId)
+  FOREIGN KEY (characteristic_id)
 	  REFERENCES characteristics(id)
 	  ON DELETE CASCADE,
   FOREIGN KEY (reviewId)
