ef5749327b8b8d3974b6c91a3fd285e87804b378
Momo Sun <mosun1113@gmail.com>
2021-03-22 13:32:10 -0700

    re-construct folder

--- Database/db.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '123456789',
-    database: 'reviews_api'
-});
-
-module.exports.connection = connection;
