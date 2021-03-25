fce26889c19152fe469aff07bd434dd5da2dbb73
Momo Sun <mosun1113@gmail.com>
2021-03-24 19:48:54 -0700

    Final itereation with docker-compose

--- Reviews_Api/Database/db.js
+++ Reviews_Api/Database/db.js
@@ -2,8 +2,10 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '123456789',
+    //password: '123456789',
     database: 'reviews_api'
 });
 
 module.exports.connection = connection;
+
+//MYSQL_HOST_IP: '192.168.2.8'
